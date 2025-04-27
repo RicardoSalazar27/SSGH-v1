9b4712c99309da9c82d5bfa820ef83096bd5c13e
ricardosexenes <edgar_ss2012@hotmail.com>
2025-04-27 15:26:16 -0600

    aplicaremos funciones para proteger las rutas

--- includes/funciones.php
+++ includes/funciones.php
@@ -18,6 +18,24 @@ function is_auth() : bool{
     return isset($_SESSION['nombre']) && !empty($_SESSION);
 }
 
+// function tiene_rol(String $rol_id): bool {
+//     if(!$_SESSION['rol_id'] || $_SESSION['rol_id'] !== $rol_id){
+//         header('Location: /');
+//     }
+//     return true;
+// }
+
+function tiene_rol(array $roles): bool {
+    // Verificar si el rol del usuario está en el arreglo de roles permitido
+    if (!isset($_SESSION['rol_id']) || !in_array($_SESSION['rol_id'], $roles)) {
+        // Redirigir si no tiene el rol necesario
+        header('Location: /acceso-denegado');  // O la ruta que quieras para acceso denegado
+        exit;
+    }
+    return true;
+}
+
+
 function respuesta($tipo, $titulo, $mensaje){
     return [
         "tipo" => $tipo,
