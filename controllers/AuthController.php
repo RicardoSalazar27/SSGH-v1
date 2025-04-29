a8840e77e903268ac9d494e761f5820a4f58b8bc
ricardosexenes <edgar_ss2012@hotmail.com>
2025-04-29 10:05:28 -0600

    arregle el head de la app

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -136,107 +136,6 @@ class AuthController {
         ]);
     }
     
-    
-
-    public static function olvide(Router $router) {
-        $alertas = [];
-
-        $hotel = Hotel::get(1);
-        
-        if($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $usuario = new Usuario($_POST);
-            $alertas = $usuario->validarEmail();
-
-            if(empty($alertas)) {
-                // Buscar el usuario
-                $usuario = Usuario::where('email', $usuario->email);
-
-                if($usuario && $usuario->confirmado) {
-
-                    // Generar un nuevo token
-                    $usuario->crearToken();
-                    unset($usuario->password2);
-
-                    // Actualizar el usuario
-                    $usuario->guardar();
-
-                    // Enviar el email
-                    $email = new Email( $usuario->email, $usuario->nombre, $usuario->token );
-                    $email->enviarInstrucciones();
-
-
-                    // Imprimir la alerta
-                    // Usuario::setAlerta('exito', 'Hemos enviado las instrucciones a tu email');
-
-                    $alertas['exito'][] = 'Hemos enviado las instrucciones a tu email';
-                } else {
-                 
-                    // Usuario::setAlerta('error', 'El Usuario no existe o no esta confirmado');
-
-                    $alertas['error'][] = 'El Usuario no existe o no esta confirmado';
-                }
-            }
-        }
-
-        // Muestra la vista
-        $router->render('auth/olvide', [
-            'titulo' => 'Olvide mi Password',
-            'hotel' => $hotel
-        ]);
-    }
-
-    public static function reestablecer(Router $router) {
-
-        $token = s($_GET['token']);
-
-        $token_valido = true;
-
-        if(!$token) header('Location: /');
-
-        // Identificar el usuario con este token
-        $usuario = Usuario::where('token', $token);
-
-        if(empty($usuario)) {
-            Usuario::setAlerta('error', 'Token No Válido, intenta de nuevo');
-            $token_valido = false;
-        }
-
-
-        if($_SERVER['REQUEST_METHOD'] === 'POST') {
-
-            // Añadir el nuevo password
-            $usuario->sincronizar($_POST);
-
-            // Validar el password
-            $alertas = $usuario->validarPassword();
-
-            if(empty($alertas)) {
-                // Hashear el nuevo password
-                $usuario->hashPassword();
-
-                // Eliminar el Token
-                $usuario->token = null;
-
-                // Guardar el usuario en la BD
-                $resultado = $usuario->guardar();
-
-                // Redireccionar
-                if($resultado) {
-                    header('Location: /');
-                }
-            }
-        }
-
-        $alertas = Usuario::getAlertas();
-        
-        // Muestra la vista
-        $router->render('auth/reestablecer', [
-            'titulo' => 'Reestablecer Password',
-            'alertas' => $alertas,
-            'token_valido' => $token_valido
-        ]);
-    }
-
     public static function mensaje(Router $router) {
 
         $router->render('auth/mensaje', [
@@ -253,35 +152,132 @@ class AuthController {
         ]);
     }
 
-    public static function confirmar(Router $router) {
+    // public static function olvide(Router $router) {
+    //     $alertas = [];
+
+    //     $hotel = Hotel::get(1);
         
-        $token = s($_GET['token']);
+    //     if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    //         $usuario = new Usuario($_POST);
+    //         $alertas = $usuario->validarEmail();
 
-        if(!$token) header('Location: /');
+    //         if(empty($alertas)) {
+    //             // Buscar el usuario
+    //             $usuario = Usuario::where('email', $usuario->email);
 
-        // Encontrar al usuario con este token
-        $usuario = Usuario::where('token', $token);
+    //             if($usuario && $usuario->confirmado) {
 
-        if(empty($usuario)) {
-            // No se encontró un usuario con ese token
-            Usuario::setAlerta('error', 'Token No Válido');
-        } else {
-            // Confirmar la cuenta
-            $usuario->confirmado = 1;
-            $usuario->token = '';
-            unset($usuario->password2);
+    //                 // Generar un nuevo token
+    //                 $usuario->crearToken();
+    //                 unset($usuario->password2);
+
+    //                 // Actualizar el usuario
+    //                 $usuario->guardar();
+
+    //                 // Enviar el email
+    //                 $email = new Email( $usuario->email, $usuario->nombre, $usuario->token );
+    //                 $email->enviarInstrucciones();
+
+
+    //                 // Imprimir la alerta
+    //                 // Usuario::setAlerta('exito', 'Hemos enviado las instrucciones a tu email');
+
+    //                 $alertas['exito'][] = 'Hemos enviado las instrucciones a tu email';
+    //             } else {
+                 
+    //                 // Usuario::setAlerta('error', 'El Usuario no existe o no esta confirmado');
+
+    //                 $alertas['error'][] = 'El Usuario no existe o no esta confirmado';
+    //             }
+    //         }
+    //     }
+
+    //     // Muestra la vista
+    //     $router->render('auth/olvide', [
+    //         'titulo' => 'Olvide mi Password',
+    //         'hotel' => $hotel
+    //     ]);
+    // }
+
+    // public static function reestablecer(Router $router) {
+
+    //     $token = s($_GET['token']);
+
+    //     $token_valido = true;
+
+    //     if(!$token) header('Location: /');
+
+    //     // Identificar el usuario con este token
+    //     $usuario = Usuario::where('token', $token);
+
+    //     if(empty($usuario)) {
+    //         Usuario::setAlerta('error', 'Token No Válido, intenta de nuevo');
+    //         $token_valido = false;
+    //     }
+
+
+    //     if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    //         // Añadir el nuevo password
+    //         $usuario->sincronizar($_POST);
+
+    //         // Validar el password
+    //         $alertas = $usuario->validarPassword();
+
+    //         if(empty($alertas)) {
+    //             // Hashear el nuevo password
+    //             $usuario->hashPassword();
+
+    //             // Eliminar el Token
+    //             $usuario->token = null;
+
+    //             // Guardar el usuario en la BD
+    //             $resultado = $usuario->guardar();
+
+    //             // Redireccionar
+    //             if($resultado) {
+    //                 header('Location: /');
+    //             }
+    //         }
+    //     }
+
+    //     $alertas = Usuario::getAlertas();
+        
+    //     // Muestra la vista
+    //     $router->render('auth/reestablecer', [
+    //         'titulo' => 'Reestablecer Password',
+    //         'alertas' => $alertas,
+    //         'token_valido' => $token_valido
+    //     ]);
+    // }
+
+    // public static function confirmar(Router $router) {
+        
+    //     $token = s($_GET['token']);
+
+    //     if(!$token) header('Location: /');
+
+    //     // Encontrar al usuario con este token
+    //     $usuario = Usuario::where('token', $token);
+
+    //     if(empty($usuario)) {
+    //         // No se encontró un usuario con ese token
+    //         Usuario::setAlerta('error', 'Token No Válido');
+    //     } else {
+    //         // Confirmar la cuenta
+    //         $usuario->confirmado = 1;
+    //         $usuario->token = '';
+    //         unset($usuario->password2);
             
-            // Guardar en la BD
-            $usuario->guardar();
+    //         // Guardar en la BD
+    //         $usuario->guardar();
 
-            Usuario::setAlerta('exito', 'Cuenta Comprobada Correctamente');
-        }
+    //         Usuario::setAlerta('exito', 'Cuenta Comprobada Correctamente');
+    //     }
 
-     
-
-        $router->render('auth/confirmar', [
-            'titulo' => 'Confirma tu cuenta DevWebcamp',
-            'alertas' => Usuario::getAlertas()
-        ]);
-    }
+    //     $router->render('auth/confirmar', [
+    //         'titulo' => 'Confirma tu cuenta DevWebcamp',
+    //         'alertas' => Usuario::getAlertas()
+    //     ]);
+    // }
 }
