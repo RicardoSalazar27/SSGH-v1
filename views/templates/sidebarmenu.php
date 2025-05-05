32ec3a2c0d71adb953ae47a75b5280515202fd5e
ricardosexenes <edgar_ss2012@hotmail.com>
2025-05-05 13:58:23 -0600

    correguimos sidebar

--- views/templates/sidebarmenu.php
+++ views/templates/sidebarmenu.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
 
-    <a href="index3.html" class="brand-link" style="text-decoration: none;">
+    <a class="brand-link" style="text-decoration: none;">
         <img 
             src="/build/img/<?php echo empty($hotel->img) ? 'tulogo' : $hotel->img; ?>.png" 
             alt="imagen-hotel" 
